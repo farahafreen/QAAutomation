aa6fa14c0e90c42a4b9bb12d6911b8304829ac9e
farahafreen <farahafreen9897@gmail.com>
2026-03-29 17:31:06 -0400

    Factorial of number

---
+++ TestAutomation/src/com/test/auto/FactorialOfNumb.java
@@ -0,0 +1,23 @@
+package com.test.auto;
+
+public class FactorialOfNumb {
+	public static void main(String[] args) {
+		int numb=3;
+		int fact=1;
+		while (numb>0)
+		{
+		fact=fact*numb;
+		numb=numb-1;
+		}
+		
+		System.out.println("factorial of number"+fact);
+		
+		}
+}
+		
+		
+	
+		
+	
+
+
